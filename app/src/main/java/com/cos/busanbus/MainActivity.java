6e2781f301af5ac477b2645df08bd0b489d73a7d
Jung-jinwoo <wlsdn960319@gmail.com>
2021-09-01 16:59:10 +0900

    test

--- app/src/main/java/com/cos/busanbus/MainActivity.java
+++ app/src/main/java/com/cos/busanbus/MainActivity.java
@@ -1,64 +1,253 @@
 package com.cos.busanbus;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.opencsv.CSVReader;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.lang.reflect.Array;
 
-import lombok.val;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
+import net.daum.mf.map.api.MapView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mBusEditText;
-    private TextView mResult;
+    private static final String LOG_TAG = "MainActivity";
+
+    private MainActivity mContext = this;
+    private MapView mapView;
+    private ViewGroup mapViewContainer;
+
+    private EditText edit;
+    private TextView text;
+    private Button search;
+    taskDataBustop td;
+    String key = "http://61.43.246.153/openapi-data/service/busanBIMS2/busStop?arsno=13123&pageNo=1&numOfRows=10&ServiceKey=FDNF6JMfFFTFBHH2nJKqLNKoJMl1VumD3J1LC7xcc%2BuB8I2uLgYWh83d%2FNZ%2FaUHGk%2BgT0BChBBcuds7YYcvXqw%3D%3D";
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBusEditText = findViewById(R.id.busEdit);
-        mResult = findViewById(R.id.result);
+        edit = findViewById(R.id.editSearch);
+        text = findViewById(R.id.result);
+        search = findViewById(R.id.searchButton);
+
+        mapView = new MapView(mContext);
+        mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        // 중심점 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.15601700819226, 129.0594790151887), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(3, true);
+
+        // 중심점 변경 + 줌 레벨 변경
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(35.15601700819226, 129.0594790151887), 2, true);
+
+        // 줌 인
+        mapView.zoomIn(true);
+
+        // 줌 아웃
+        mapView.zoomOut(true);
+
+        //마커 1
+        MapPOIItem marker = new MapPOIItem();
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(35.15601700819226,129.0594790151887);
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+//      mapView.addPOIItem(marker);
+
+        //마커 2
+        MapPOIItem marker2 = new MapPOIItem();
+        MapPoint MARKER_POINT2 = MapPoint.mapPointWithGeoCoord(35.15601700819226,129.0594790151887);
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT2);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker2);
+
+        MapPOIItem [] markers = new MapPOIItem[2];
+        markers[0]=marker;
+        markers[1]=marker2;
 
 
-        final BusDb db = Room.databaseBuilder(this,BusDb.class,"Bus-db")
-                .allowMainThreadQueries()
-                .build();
+        for(int i=0;i<markers.length;i++){
+            mapView.addPOIItem(markers[i]);
+        }
 
-        mResult.setText(db.busDao().findAll().toString());
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        edit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                /*db.busDao().insert(new Bus(mBusEditText.getText().toString()));
-                mResult.setText(db.busDao().findAll().toString());*/
+            public void onClick(View v) {
+
             }
         });
 
+
+        search.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                td = new taskDataBustop();
+                td.execute(edit.getText().toString());
+                edit.setText("");
+            }
+        });
+
+        /*getHashKey(); // 해시키 받는 함수*/
     }
 
-    public void readCSV(String filePath) throws IOException {
-        CSVReader cr = new CSVReader(new FileReader(filePath));
-        String [] nextLine;
-        while ((nextLine = cr.readNext()) != null){
-            for(int i=0; i<nextLine.length; i++){
-                System.out.println(nextLine[i]);
+
+    /*public class searchView extends View{
+
+        public searchView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_MOVE){
+                return true;
+            }else{
+                return false;
             }
-            System.out.println();
+        }
+    }*/
+
+    public class taskDataBustop extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... search) {
+            String str = edit.getText().toString();
+            String url = "http://61.43.246.153/openapi-data/service/busanBIMS2/busStop?resultCode=00&pageNo=1&numOfRows=10&ServiceKey=FDNF6JMfFFTFBHH2nJKqLNKoJMl1VumD3J1LC7xcc%2BuB8I2uLgYWh83d%2FNZ%2FaUHGk%2BgT0BChBBcuds7YYcvXqw%3D%3D";
+            XmlPullParserFactory factory;
+            XmlPullParser parser;
+            URL xmlUrl;
+            String returnResult = "";
+            String gpsX = "";
+            String gpsY = "";
+            String searchData = "";
+            try {
+                boolean flag1 = false;
+                boolean flagGPSX = false;
+                boolean flagGPSY = false;
+
+                xmlUrl = new URL(url);
+                xmlUrl.openConnection().getInputStream();
+                factory = XmlPullParserFactory.newInstance();
+                parser = factory.newPullParser();
+                parser.setInput(xmlUrl.openStream(), "utf-8");
+                int eventType = parser.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    switch (eventType) {
+                        case XmlPullParser.START_DOCUMENT:
+                            break;
+                        case XmlPullParser.END_DOCUMENT:
+                            break;
+                        case XmlPullParser.START_TAG:
+                            if (parser.getName().equals("bstopArsno")) {
+                                flag1 = true;
+                            } else if(parser.getName().equals("bstopNm")){
+                                flag1 = true;
+                            } else if(parser.getName().equals("gpsX")){
+                                flagGPSX = true;
+                            } else if(parser.getName().equals("gpsY")){
+                                flagGPSY = true;
+
+                            }
+                            break;
+                        case XmlPullParser.END_TAG:
+                            break;
+                        case XmlPullParser.TEXT:
+                            if (flag1 == true) {
+                                returnResult += parser.getText() + "\n";
+                                flag1 = false;
+
+                            } else if(flagGPSX == true){
+                                gpsX = parser.getText() + "/n";
+                                System.out.println("X좌표: " + gpsX);
+                                flagGPSX = false;
+
+                            } else if(flagGPSY == true){
+                                gpsY = parser.getText() + "/n";
+                                System.out.println("Y좌표: " + gpsY);
+                                flagGPSY = false;
+                            }
+                            break;
+                    }
+                    eventType = parser.next();
+                }
+            } catch (Exception e) {
+
+            }
+
+            System.out.println("totalResult : " + returnResult);
+            return returnResult;
+            /*for(int i=0; i<returnResult.length(); i++){
+                if(returnResult.equals(edit.getText())){
+                    searchData += edit.getText() + "/n";
+                }
+            }
+
+            System.out.println("searchData : " + searchData);*/
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            text.setText(result);
         }
     }
+
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
 }
